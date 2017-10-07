a63782e9da0d47e7a4f65a08303f0ca634e76794
Thanh Quan <quan.hnt3777@sinhvien.hoasen.edu.vn>
2017-10-07 20:32:24 +0700

    Auto change slide in 3s

--- JS/slider/js/javascript.js
+++ JS/slider/js/javascript.js
@@ -1,15 +1,26 @@
-//Current slide variable
+/**
+ * Image slider
+ * @author quan.hnt3777@sinhvien.hoasen.edu.vn (Quan Huynh)
+ */
+
+//Variable of active image in list image
 var slideIndex = 0;
-var reTimeout;
+
 //Set default for the first slide
 showSlide(slideIndex);
+/**
+ * Display image, change image after 3s
+ * @param {int} n Ordinal number of an image in list image
+ */
+setInterval(function () {
+	showSlide(slideIndex +=1);
+}, 3000);
 
 function showSlide(n) {
 	var i;
-	var slide = document.getElementsByClassName("current-image");
-	var listImage = document.getElementsByClassName("image");
-	//slideIndex : 0~3; slide.length : 1~4
-	//Move from the last slide to the first
+	var slide = document.getElementsByClassName("slide");
+	var listImage = document.getElementsByClassName("list-image");
+	//Move from the last slide to the first (n = 4)
 	if (n >= slide.length) {
 		slideIndex = 0;
 	}
@@ -17,29 +28,31 @@ function showSlide(n) {
 	if (n < 0) {
 		slideIndex = slide.length - 1;
 	}
+	//
 	for (i = 0; i < slide.length; i++) {
 		slide[i].style.display = "none";
 	}
 	for (i = 0; i < listImage.length; i++) {
 		listImage[i].className = listImage[i].className.replace(" active", ""); 
 	}
-	//Change display's value of style attribute from "none" to "block"
+	//Change from "none" to "block"
 	slide[slideIndex].style.display = "block";
 	//Border current image with class "active"
 	listImage[slideIndex].className += " active";
-	/*
-	clearTimeout(reTimeout);
-	//Time (3s) move to next slide
-	reTimeout = setTimeout(function () {
-		showSlide(slideIndex +=1);
-	}, 3000);
-	*/
 }
-//Choose image to see on the slide
+
+/**
+ * Choose an image from list image, display it on the slide
+ * @param {int} n Ordinal number of an image in list image
+ */
 function currentSlide(n) {
 	showSlide(slideIndex = n);
 }
-//Click on Previous image (<) or Next image (>)
+
+/**
+ * Click on Previous image (<) or Next image (>)
+ * @param {int} n Ordinal number of an image in list image
+ */
 function adjacentImg(n) {
 	showSlide(slideIndex += n);
 }
