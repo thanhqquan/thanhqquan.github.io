99824902106278f7f6965c013b7366550a8e0fc3
Thanh Quan <quan.hnt3777@sinhvien.hoasen.edu.vn>
2017-11-24 17:34:39 +0700

    60% - Check username, password, email

--- jQuery/formajax/js/javascript.js
+++ jQuery/formajax/js/javascript.js
@@ -2,17 +2,13 @@
  * Form AJAX - jQuery
  * @author quan.hnt3777@sinhvien.hoasen.edu.vn (Quan Huynh)
  */
-var USERNAME = $("#username");
-/*
- * Check input Username (8 <= length <= 35)
- */
-$(USERNAME).change(function() {
-    checkUsernameLength();
-});
 
-// $('input[name=username]').change(function(){
-    // checkUsernameLength();
-// });
+/*
+ Check input Username (8 <= length <= 35)
+ */
+$($("#username").change(function() {
+    checkUsernameLength();
+}));
 
 function checkUsernameLength() {
     var tbUsername = $("#username").val(); // value of textbox username
@@ -34,7 +30,7 @@ function checkUsernameLength() {
 }
 
 /*
- * Check input Password (length >= 8)
+ Check input Password (length >= 8)
  */
 $($("#password").change(function() {
     checkPasswordLength();
@@ -57,50 +53,92 @@ function checkPasswordLength() {
 }
 
 /*
- * Check input Email (format: English, has "@", ".", may has ".", "_", "-" before "@")
+ Check input Email (format: English, has "@", ".", may has ".", "_", "-" before "@")
  */
+$($("#email").change(function() {
+    checkEmailFormat();
+}));
+
 function checkEmailFormat() {
-    var tbEmail = document.getElementById("email").value; // value of textbox email
-    var errorEmail = document.getElementById("error-email");
+    var tbEmail = $("#email").val(); // value of textbox email
+    var errorEmail = $("#error-email");
     var REGEX = /^([a-z0-9\._-]+)@([a-z]+)\.([a-z\.]{2,6})$/;
 
     if (tbEmail.length == 0) {
-        errorEmail.innerHTML = "Please enter email";
+        errorEmail.html("Please enter email");
         return false;
     } else if (!REGEX.test(tbEmail)) {
-        errorEmail.innerHTML = "Email wrong format";
+        errorEmail.html("Email wrong format");
         return false;
     } else {
-        errorEmail.innerHTML = "";
+        errorEmail.html("");
         return true;
     }
 }
 
 /*
- * Check input Birthday (birthday <= now)
+ Check input Birthday (valid: birthday <= now)
  */
+$($("#picked-day").change(function() {
+    checkBirthdayFormat();
+}));
+
 function checkBirthdayFormat() {
-    var tbBirthday = document.getElementById("picked-day").value; // value of birthday
+    var tbBirthday = $("#picked-day").val(); // value of birthday
     var myDate = new Date(tbBirthday);
     var now = new Date();
-    var calendar = document.getElementById("table-calendar");
-    calendar.style.display = "none";
-    var errorBirthday = document.getElementById("error-birthday");
+    var calendar = $("#table-calendar");
+    calendar.css("display", "none");
+    var errorBirthday = $("#error-birthday");
 
     if (myDate > now) {
-        errorBirthday.innerHTML = "Birthday must be on or before today";
+        errorBirthday.html("Birthday must be on or before today");
         return false;
     } else {
-        errorBirthday.innerHTML = "";
+        errorBirthday.html("");
         return true;
     }
 }
 
 /*
- * Display calendar when clicked on textbox picked-day
+ Display calendar when clicked on textbox picked-day
  */
 $(function() {
     $("#picked-day").on("click", function() {
         $("#table-calendar").css("display", "block");
     });
 });
+
+/*
+ Validate information before submit to server 
+ */
+function submitForm() {
+    var xhttp; // XMLHttpRequest
+    document.getElementById("status").innerHTML = ""; // status clear when clicked button "SUBMIT"
+    var tbUsername = document.getElementById("username").value;
+    var tbPassword = document.getElementById("password").value;
+    var tbEmail = document.getElementById("email").value;
+    var tbBirthday = document.getElementById("picked-day").value;
+    var data = "username=" + tbUsername + "&password=" + tbPassword + "&email=" + tbEmail + "&birthday=" + tbBirthday;
+
+    if (checkUsernameLength() && checkPasswordLength() && checkEmailFormat() && checkBirthdayFormat()) {
+        if (window.XMLHttpRequest) {
+            // code for modern browsers
+            xhttp = new XMLHttpRequest();
+        } else {
+            // code for IE6, IE5
+            xhttp = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                document.getElementById("status").innerHTML = xhttp.responseText;
+            }
+        };
+        
+        // Send data to Add_Db.php
+        xhttp.open("POST", "Add_Db.php", true);
+        xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        document.getElementById("status").innerHTML = "Processing..."; // status when form sending to server
+        xhttp.send(data);
+    } else { return false; }
+}
